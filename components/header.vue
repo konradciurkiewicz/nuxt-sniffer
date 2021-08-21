1417bb4e9a22cabb669845fe38e7c9a16816679c
Konrad Ciurkiewicz <konradciurkiewicz@wp.pl>
2021-08-21 13:19:51 +0200

    init

---
+++ components/header.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row w="12">
+    <vs-col w="3">
+      asdasd
+    </vs-col>
+  </v-row>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <div class="grid">
+          <Header></Header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/header";
+export default {
+  components: {Header},
+  layout: 'default'
+}
+</script>
